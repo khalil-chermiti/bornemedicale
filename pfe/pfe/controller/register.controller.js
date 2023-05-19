d2310671e7258735dee84ab5a004d283518386f1
khalil-chermiti <khalil666chermiti@gmail.com>
2023-05-19 17:55:15 +0100

    fix: fix register functionality

--- pfe/pfe/controller/register.controller.js
+++ pfe/pfe/controller/register.controller.js
@@ -1,34 +1,47 @@
-const {createUser , phoneExists} = require('../models/register.model');
-const {JWT_SECRET} = require('../utils/config');
-const jwt = require('jsonwebtoken');
+const { createUser, phoneExists } = require("../models/register.model");
+const { JWT_SECRET } = require("../utils/config");
+const jwt = require("jsonwebtoken");
 
 // encrypting password
-async function registerUser (req,res){
-    const user = req.body;
-    //check valid input
-    if(!user || !user.firstName || !user.lastName || !user.phoneNumber || !user.email || !user.birthDate || !user.gender){
-        console.log(user)
-        return res.status(403).json({"message" : "Veuillez remplir tous les champs"});
-    }
-    //check for unique phoneNumber
-    let phoneNumber = await phoneExists(user.numberPhone);
-    if(phoneNumber.length>0) {
-        return res.status(403).send({"message": "username is taken"});
-    }
+async function registerUser(req, res) {
+  const user = req.body;
 
-    //creating user
-    const userData = await createUser(user);
-    console.log(userData)
-    let id = userData.id;
-    //creating token 
-    const token = jwt.sign({id}, JWT_SECRET , { expiresIn: '1h'});
+  console.log(user);
 
+  //check valid input
+  if (
+    !user ||
+    !user.firstName ||
+    !user.lastName ||
+    !user.phoneNumber ||
+    !user.email ||
+    !user.birthDate ||
+    !user.gender
+  ) {
     return res
-        .cookie('access_token' , token)
-        .status(200)
-        .json({token : token});
+      .status(403)
+      .json({ message: "Veuillez remplir tous les champs" });
+  }
+  //check for unique phoneNumber
+  let phoneNumber = await phoneExists(user.numberPhone);
+  if (phoneNumber.length > 0) {
+    return res.status(403).send({ message: "username is taken" });
+  }
 
+  //creating user
+  let userData;
+  try {
+    userData = await createUser(user);
+  } catch (error) {
+    console.log("error creating user", error);
+  }
+
+  let id = userData.id;
+  //creating token
+  const token = jwt.sign({ id }, JWT_SECRET, { expiresIn: "1h" });
+
+  return res.cookie("access_token", token).status(200).json({ token: token });
 }
 module.exports = {
-    registerUser,
-}
+  registerUser,
+};
