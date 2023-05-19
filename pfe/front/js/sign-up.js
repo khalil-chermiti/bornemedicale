fc10081ff67cff87ace6c4e26ccc113cccf1e9d8
khalil-chermiti <khalil666chermiti@gmail.com>
2023-05-19 17:55:15 +0100

    fix: fix register functionality

--- pfe/front/js/sign-up.js
+++ pfe/front/js/sign-up.js
@@ -1,54 +1,60 @@
-const API_URL = "http://localhost:3000"
-const signUpButton = document.getElementById("sign-up")
+const API_URL = "http://localhost:3000";
+const signUpButton = document.getElementById("sign-up");
 let firstName = document.getElementById("prenom");
 let lastName = document.getElementById("nom");
 let phoneNumber = document.getElementById("numberphone");
-let email = document.getElementById("email")
-let birthDate = document.getElementById("date-n")
+let email = document.getElementById("email");
+let birthDate = document.getElementById("date-n");
 let isMale = document.getElementById("h");
 let isFemale = document.getElementById("f");
 
+// signup initial state
+const signupData = {
+  firstName: null,
+  lastName: null,
+  phoneNumber: null,
+  email: null,
+  birthDate: null,
+  gender: "m",
+};
 
-firstName.addEventListener("change",(e)=>{
-    firstName = e.target.value;
-})
+firstName.addEventListener("change", e => {
+  signupData.firstName = e.target.value;
+});
 
-lastName.addEventListener("change",(e)=>{
-    lastName = e.target.value;
-})
+lastName.addEventListener("change", e => {
+  signupData.lastName = e.target.value;
+});
 
-phoneNumber.addEventListener("change",(e)=>{
-    phoneNumber = e.target.value;
-})
+phoneNumber.addEventListener("change", e => {
+  signupData.phoneNumber = e.target.value;
+});
 
-email.addEventListener("change",(e)=>{
-    email = e.target.value;
-})
+email.addEventListener("change", e => {
+  signupData.email = e.target.value;
+});
 
-birthDate.addEventListener("change",(e)=>{
-    birthDate = e.target.value;
-})
+birthDate.addEventListener("change", e => {
+  signupData.birthDate = e.target.value;
+});
 
-signUpButton.addEventListener("click",async (e)=>{
-    try{
- const response = await fetch(`${API_URL}/register`,{
-        method:'POST',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body:JSON.stringify({
-            firstName,
-            lastName,
-            phoneNumber,
-            email,
-            birthDate,
-            gender:isMale.ariaChecked ? "M" : "F"
-        }),
-    })
+signUpButton.addEventListener("click", async e => {
+  console.log(signupData);
+  try {
+    const response = await fetch(`${API_URL}/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...signupData,
+        gender: isMale.checked ? "m" : "f",
+      }),
+    });
 
     if(response.ok) window.location.href = "login.html"
-    console.log(response);
-    }catch(e){
-        console.log(e);
-    }
-})
+    console.log(await response.json());
+  } catch (e) {
+    console.log(e);
+  }
+});
